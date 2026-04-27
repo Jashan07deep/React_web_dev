4788860bcb870e61ef557f734297e38cc919b273
Jashandeep <jashan.07deep2@gmail.com>
2026-04-27 09:48:59 +0530

    27-04

--- reactapp/src/component/HomePage.jsx
+++ reactapp/src/component/HomePage.jsx
@@ -1,4 +1,25 @@
+// import React,{useState} from 'react'
+// import 
+
+// function HomePage() {
+//     const [login, setLogin] = useState(false)
+//   if(login){
+//     return<><button onClick={()=>setLogin(false)}>Logout</button></>
+//   }else{
+//     return<><button onClick={()=>setLogin(true)}>Login</button></>
+//   }
+
+//   return (
+//     <>
+//     {login?<button onClick={()=>setLogin(false)}>Logout</button>:<button onClick={()=>setLogin(true)}>Login</button>}
+//     </>
+//   )
+// }
+
+// export default HomePage
+
 import React,{useState} from 'react'
+import { Link } from 'react-router-dom'
 
 function HomePage() {
     const [login, setLogin] = useState(false)
@@ -11,6 +32,10 @@ function HomePage() {
   return (
     <>
     {login?<button onClick={()=>setLogin(false)}>Logout</button>:<button onClick={()=>setLogin(true)}>Login</button>}
+    <ul>
+      <li><Link to="/">HomePage</Link></li>
+      <li><Link to="/contact">Contact</Link></li>
+    </ul>
     </>
   )
 }
